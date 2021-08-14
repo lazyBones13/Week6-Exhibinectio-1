2d34124bf89a390dc4fe96bac4a24c78ee41ae81
JM <s9923024@gmail.com>
2021-08-14 23:29:37 +0800

    首頁跟RWD完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,8 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
