007ce7f2421a5016fd722fb66c73885bfdf8e326
JM <s9923024@gmail.com>
2021-08-22 01:42:24 +0800

    完成首頁、設計分頁及產品內頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,8 +1,10 @@
 var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 })
