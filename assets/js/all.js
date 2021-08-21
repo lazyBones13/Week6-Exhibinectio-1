37056b32812469a7497187dae000b9fc6fc3328d
JM <s9923024@gmail.com>
2021-08-22 01:41:00 +0800

    Update 2021-08-21T17:40:56.635Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,13 @@
 "use strict";
 
 var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
   }
 });
 //# sourceMappingURL=all.js.map
